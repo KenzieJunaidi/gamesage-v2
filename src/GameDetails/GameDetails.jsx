5b81374c9de290cc4f1c946fa1536996096ce4bc
Kenzie Junaidi <kenzie.junaidi2@gmail.com>
2025-09-25 14:41:34 +0700

    Finished GameDetails UI

--- src/GameDetails/GameDetails.jsx
+++ src/GameDetails/GameDetails.jsx
@@ -13,20 +13,37 @@ import "./GameDetails.css";
 
 export const GameDetails = ({ game, setSelectedGame }) => {
 
+    const [appear, setAppear] = useState(false);
     const [currentImage, setCurrentImage] = useState(null);
 
     useEffect(() => {
         if(game){
             setCurrentImage(game.screenshots[0]);
+            setAppear(true);
+        }
+        else{
+            setAppear(false);
         }
     }, [game])
 
     if(!game){
-        return null;
+        return null
     }
 
+    const review_color = {
+        "Overwhelmingly Positive": "#0F7CEB",
+        "Very Positive": "#3399FF",
+        "Positive": "#4A90E2",
+        "Mostly Positive": "#4A90E2",
+        "Mixed": "#FFC300",
+        "Mostly Negative": "#E2574C",
+        "Negative": "#E2574C",
+        "Very Negative": "#C0392B",
+        "Overwhelmingly Negative": "#C0392B",
+    };
+
     return(
-        <motion.div className="detail-section">
+        <motion.div className={`detail-section ${appear ? "display" : ""}`}>
             <motion.div className="detail-card">
                 <div className="back-btn" onClick={() => setSelectedGame(null)}>
                     <i class="fa-solid fa-arrow-left"></i>
@@ -50,35 +67,37 @@ export const GameDetails = ({ game, setSelectedGame }) => {
                     </motion.div>
 
                     <div className="item-detail">
-                        <h1>{game.name}</h1>
-                        <h3>{game.short_description}</h3>
-                        <h4>{game.price === 0 ? "Free" : `Rp${Number(game.price).toLocaleString("id-ID")}`}</h4>
+                        <h1 className="item-name">{game.name}</h1>
+                        <h3 className="item-desc">{game.short_description}</h3>
+                        <h4 className="item-price">{game.price === 0 ? "Free" : `Rp${Number(game.price).toLocaleString("id-ID")}`}</h4>
 
-                        <div>
-                            <p>REVIEWS: </p>
-                            <p>{game.review_score_text} <span>({game.review_count.toLocaleString()})</span></p>
+                        <div className="classifier-wrapper">
+                            <p className="classifier">REVIEWS: </p>
+                            <p style={{ color: (review_color[game.review_score_text] || "var(--text-color)"), fontWeight: "700" }}>{game.review_score_text} <span style={{ color: "var(--text-color)", fontWeight: "400" }}>({game.review_count.toLocaleString()})</span></p>
                         </div>
-                        <div>
-                            <p>RELEASE DATE: </p>
+                        <div className="classifier-wrapper" style={{ marginBottom: "2rem" }}>
+                            <p className="classifier">RELEASE DATE: </p>
                             <p>{game.release_date}</p>
                         </div>
 
-                        <div>
-                            <p>DEVELOPER: </p>
+                        <div className="classifier-wrapper">
+                            <p className="classifier">DEVELOPER: </p>
                             <p>{game.developers[0]}</p>
                         </div>
-                        <div>
-                            <p>PUBLISHER: </p>
+                        <div className="classifier-wrapper" style={{ marginBottom: "2rem" }}>
+                            <p className="classifier">PUBLISHER: </p>
                             <p>{game.publishers[0]}</p>
                         </div>
 
                         <div>
-                            <p>GENRES</p>
-                            {game.genres.map((genre, idx) => (
-                                <div key={idx}>
-                                    <p>{genre}</p>
-                                </div>
-                            ))}
+                            <p className="classifier">GENRES</p>
+                            <div className="item-genre-wrapper">
+                                {game.genres.map((genre, idx) => (
+                                    <div key={idx} className="item-genre">
+                                        <p>{genre}</p>
+                                    </div>
+                                ))}
+                            </div>
                         </div>
                     </div>
                 </motion.div>
