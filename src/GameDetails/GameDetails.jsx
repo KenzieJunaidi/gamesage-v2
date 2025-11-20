c2bdeb7580ba26dc168a244cadba8c4122465fe7
Kenzie Junaidi <kenzie.junaidi2@gmail.com>
2025-11-20 13:28:00 +0700

    Minor Code Cleanup

--- src/GameDetails/GameDetails.jsx
+++ src/GameDetails/GameDetails.jsx
@@ -1,13 +1,5 @@
 import { motion } from "framer-motion";
 import { useEffect, useState } from "react";
-import { Swiper, SwiperSlide } from "swiper/react"; 
-import { Thumbs, FreeMode, Navigation } from "swiper/modules";
-
-// Import Swiper styles
-import "swiper/css";
-import "swiper/css/free-mode";
-import "swiper/css/navigation";
-import "swiper/css/thumbs";
 
 import "./GameDetails.css";
 
